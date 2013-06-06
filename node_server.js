5e24d0f0f7efc6558facbffd438b46255cb2a886
Anand Patel <anandmpatel@gmail.com>
2013-06-06 13:54:58 +0530

    added flag for diferentitating unit and tower in unit class

--- node_server.js
+++ node_server.js
@@ -40,10 +40,10 @@ io.sockets.on('connection', function (socket) {
     }
 
     socket.on('setClientDetails', function (data) {
-        var data2 = {};
-        socket[data_namespace].clientName = data.name;
-        socket[data_namespace].clientId = data.id.toString();
         console.log("setting client name and id [" + data.name + ":" + data.id +"]");
+        var data2 = {};
+//        socket[data_namespace].clientName = data.name;
+//        socket[data_namespace].clientId = data.id.toString();
         socket[data_namespace].player = new Player(data.name, data.id, true);
         if(true) {
 			/*
@@ -77,6 +77,9 @@ io.sockets.on('connection', function (socket) {
             });
 
             data2.existingRooms = io.sockets.manager.rooms;
+			
+			// join my own room first
+			socket.join(socket[data_namespace].player.name);
             socket.emit('registered', data2);
         } else {
             console.log("COUCHBASE GLOBAL BUCKET NOT AVAILABLE!!");
