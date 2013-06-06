721ae29398d74bf0de343502c8909e60d08e574f
Srinath Ramanujan <gr8srinath@gmail.com>
2013-06-06 13:47:48 +0530

    Adding android project which only has a webview

---
+++ Android/src/com/fwf/fightwithfriends/MainScreen.java
@@ -0,0 +1,34 @@
+package com.fwf.fightwithfriends;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+@SuppressLint("SetJavaScriptEnabled") public class MainScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        //myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://fb.fwf.dev2-hackathon.zc2.zynga.com");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_screen, menu);
+        return true;
+    }
+    
+}
